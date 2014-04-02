d28d7042586dbb45c41ffa4a744fd9c561231aee
taekjoo <taekjoo@gmail.com>
2014-04-02 21:02:28 +0900

    to check iphone agent

--- main.rb
+++ main.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
+require 'useragent'
 
 # -------------------
-# get '/' condition by agent
-# iOS, Android, PC
+# step 1.
+# render different page after identify iOS, Android, PC
+# Step 2.
 # check app installation
 # case 1: app installed, launch app
 # case 2: app not-installed yet, redirect to app store
 
 
 get '/' do
-  "You are using #{params[:agent]}"
+  raw_user_agent = request.user_agent
+  # raw_user_agent = "Apple-iPhone5C2/1001.525"
+  # user_agent = UserAgent.parse(raw_user_agent)
+  # user_agent.platform
+  raw_user_agent
 end
